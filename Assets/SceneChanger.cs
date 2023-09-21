2189ef5ed1666efb4bf6ac4a3c64130c62bda41f
Ilan Lipsky <102115093+CoinismRS@users.noreply.github.com>
2023-09-20 20:40:59 -0500

    Update SceneChanger.cs
    - Commented out LeftClick to change scene on command

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -9,10 +9,11 @@ public class SceneChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             FadeToScene(2);
         }
+        */
 
     }
     public void FadeToScene (int levelIndex)
