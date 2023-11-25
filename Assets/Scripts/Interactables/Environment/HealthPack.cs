bb8c3866acffac661b54211657e92c953427eeb3
sawl-an-dawnen <elijah.salandanan95@gmail.com>
2023-11-24 20:27:11 -0600

    Health Pack Update
    -added health items to forest, diner, and maze
    -added more context and jump scares to diner

--- Assets/Scripts/Interactables/Environment/HealthPack.cs
+++ Assets/Scripts/Interactables/Environment/HealthPack.cs
@@ -1,22 +1,19 @@
 using UnityEngine;
 
-public class HealthPack : MonoBehaviour, CConsumable
+public class HealthPack : MonoBehaviour, IInteractable
 {
     public int heal = 10;
     private GameObject player;
     private PlayerManager playerManager;
 
-    public void Consume() {
+    public void Interact() {
         player = GameObject.FindGameObjectWithTag("Player");
         playerManager = player.GetComponent<PlayerManager>();
-        if (playerManager.health < 90)
-        {
-            playerManager.health += heal;
-        }
-        else 
+        playerManager.health += heal;
+        if (playerManager.health > 100)
         {
             playerManager.health = 100;
         }
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
 }
