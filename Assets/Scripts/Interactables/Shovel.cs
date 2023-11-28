d95e5e2a50d2178ce4d38e5c7a91e2ca7063425c
JadenJT <Jaden.thai1@gmail.com>
2023-11-28 01:08:39 -0600

    Done!

--- Assets/Scripts/Interactables/Shovel.cs
+++ Assets/Scripts/Interactables/Shovel.cs
@@ -7,16 +7,30 @@ public class Shovel : MonoBehaviour, IInteractable
     public LevelManager03 levelManager;
     public AudioClip audioClip;
     private AudioSource audioSource;
+    private GameObject[] graveTwinkles;
     // Start is called before the first frame update
 
     private void Start()
     {
+           
+       graveTwinkles = GameObject.FindGameObjectsWithTag("GT");
+        foreach (GameObject obj in graveTwinkles)
+        {
+            obj.SetActive(false);
+            Debug.Log(obj);
+        }
+
         audioSource = GameObject.FindGameObjectWithTag("Player_AudioSource_02").GetComponent<AudioSource>();
     }
 
     public void Interact() {
         levelManager.PickUpShovel();
         audioSource.PlayOneShot(audioClip);
+        foreach (GameObject obj in graveTwinkles)
+        {
+            obj.SetActive(true);
+            Debug.Log(obj);
+        }
         Destroy(gameObject);
     }
 }
